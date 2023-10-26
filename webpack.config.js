6c301a6fd2cfaea6b221f9c93c5bd2ba38032629
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-10-26 23:43:45 +0400

    feat: improve design

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'development',
+  mode: 'production',
   devtool: 'source-map',
   output: {
     filename: 'bundle.js',
